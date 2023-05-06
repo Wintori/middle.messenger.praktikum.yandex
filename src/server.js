3ac06b7b1db8d897ae6335e5cc4964540e8c679e
Egor Shashurin <egor.sh.2002@yandex.ru>
2023-05-07 00:31:33 +0300

    feat: Добавить все требования для 1 этапа

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+const dirPath = path.join(__dirname, '../', "dist");
+app.use(express.static(dirPath));
+
+app.get('/.*/', (req, res) => { return res.sendFile('./pages/index.hbs'); });
+
+app.listen(PORT, function () {
+  console.log(`Chat listening on port ${PORT}!`);
+});
