ed3e0a127891fed32f428b4bafe3f2e43aa32214
Egor Shashurin <egor.sh.2002@yandex.ru>
2023-05-08 03:13:56 +0300

    feat: Правка проекта

--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ const PORT = 3000;
 const dirPath = path.join(__dirname, '../', "dist");
 app.use(express.static(dirPath));
 
-app.get('/.*/', (req, res) => { return res.sendFile('./pages/index.hbs'); });
+app.get('/.*/', (req, res) => {
+    return res.sendFile('./pages/index.hbs');
+});
 
 app.listen(PORT, function () {
-  console.log(`Chat listening on port ${PORT}!`);
+    console.log(`Chat listening on port ${PORT}!`);
 });
