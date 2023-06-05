51ad5a08c28047c337da3258f8178952e3e6dd0d
Egor Shashurin <egor.sh.2002@yandex.ru>
2023-06-05 23:15:38 +0300

    fix: Исправить server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ const PORT = 3000;
 const dirPath = path.join(__dirname, '../', 'dist');
 app.use(express.static(dirPath));
 
-app.get('/.*/', (req, res) => res.sendFile('./index.html'));
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(dirPath, 'index.html'))
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Chat listening on port ${PORT}!`);
